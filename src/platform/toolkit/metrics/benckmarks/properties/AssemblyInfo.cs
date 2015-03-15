fecbfe591e49904d50ea48d2aaac939685d139d2
nohros <nohros.ceo@gmail.com>
2015-03-14 23:26:33 -0300

    Adapted the NodaTime benchmark tests framewor to nohros metrics framework

--- src/platform/toolkit/metrics/benckmarks/properties/AssemblyInfo.cs
+++ src/platform/toolkit/metrics/benckmarks/properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("benckmarks")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("nohros.metrics.benckmarks")]
+[assembly: AssemblyDescription("nohros.metrics.benckmarks")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nohros Inc")]
-[assembly: AssemblyProduct("benckmarks")]
-[assembly: AssemblyCopyright("Copyright © Nohros Inc 2015")]
+[assembly: AssemblyProduct("Nohros Metrics")]
+[assembly: AssemblyCopyright("Copyright © 2015 by Nohros Inc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
