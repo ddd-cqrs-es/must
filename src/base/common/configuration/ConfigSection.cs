24bdad6b65c0c31f2fc9d81c7b041a9157d130d1
nohros <nohros.ceo@gmail.com>
2015-03-25 14:46:54 -0300

    Created a helper class for ConfigSections

---
+++ src/base/common/configuration/ConfigSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+
+namespace Nohros.Configuration
+{
+  /// <summary>
+  /// A helper class for <see cref="ConfigurationSection"/>.
+  /// </summary>
+  public sealed class ConfigSection
+  {
+    /// <summary>
+    /// Gets a <see cref="ConfigurationSection"/> based on the given type.
+    /// </summary>
+    public static T GetConfiguration<T>() where T : class, new() {
+      if (!typeof (ConfigurationSection).IsAssignableFrom(typeof (T))) {
+        throw new ArgumentException(
+          "Only supports .NET ConfigurationSections is supported");
+      }
+      return ConfigurationManager.GetSection(typeof(T).Name) as T;
+    }
+  }
+}
