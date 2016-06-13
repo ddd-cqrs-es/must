4102a7c79326a732d0862e9c5fdeb262b3e99e98
nohros <nohros.ceo@gmail.com>
2016-06-13 15:19:23 -0300

    Added unit tests fo the readonly collections.

---
+++ src/tests/common/type/collection/ReadOnlyCollectionTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Nohros.Collections
+{
+  public class ReadOnlyCollectionTests
+  {
+    [Test]
+    public void should_use_original_list_as_underlying_collection() {
+      var list = new List<int> {5, 6, 7, 10};
+      var read_only = list.AsReadOnlyList();
+
+      Assert.That(read_only[0], Is.EqualTo(5));
+
+      list.Add(25);
+      Assert.That(read_only[4], Is.EqualTo(25));
+    }
+
+    [Test]
+    public void should_use_original_array_as_underlying_collection() {
+      var list = new List<int> {5, 6, 7, 10}.ToArray();
+      var read_only = list.AsReadOnlyList();
+
+      Assert.That(read_only[0], Is.EqualTo(5));
+
+      list[0] = 25;
+      Assert.That(read_only[0], Is.EqualTo(25));
+    }
+
+    [Test]
+    public void should_use_original_readonly_as_underlying_collection() {
+      var list = new List<int> {5, 6, 7, 10};
+      var sys_read_only = list.AsReadOnly();
+      var read_only = sys_read_only.AsReadOnlyList();
+
+      Assert.That(read_only[0], Is.EqualTo(5));
+
+      list[0] = 25;
+      Assert.That(read_only[0], Is.EqualTo(25));
+    }
+
+    [Test]
+    public void should_create_new_collection_if_ilist_is_not_implemented() {
+      var list = new List<int> { 5, 6, 7, 10 };
+      var read_only = list.Select(item => item).AsReadOnlyList();
+
+      Assert.That(read_only[0], Is.EqualTo(5));
+
+      list[0] = 25;
+      Assert.That(read_only[0], Is.EqualTo(5));
+    }
+  }
+}
